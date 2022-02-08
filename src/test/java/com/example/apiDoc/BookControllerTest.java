8a6fcd1ba57f45790dbfd0076934070dc5df98bd
KIM JUN SEONG <skoalp@frientrip.com>
2022-02-08 15:06:49 +0900

    Done 1. RestDocs-api-spec을 사용하여 spring rest docs를 통해 만든 결과물을 OpenAPI3 spec에 맞는 파일을 생성해냄 2. 결과물 파일은 swagger에서 사용 가능

--- src/test/java/com/example/apiDoc/BookControllerTest.java
+++ src/test/java/com/example/apiDoc/BookControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.apiDoc;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -10,8 +11,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+//import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -33,6 +37,10 @@ class BookControllerTest {
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$.id", (notNullValue()))) // (5)
+                .andExpect(jsonPath("$.title", (notNullValue())))
+                .andExpect(jsonPath("$.author", (notNullValue())))
+
                 .andDo(document("book", // (3)
                         pathParameters(
                                 parameterWithName("id").description("book unique id") // (4)
@@ -43,9 +51,25 @@ class BookControllerTest {
                                 fieldWithPath("author").description("author")
                         )
                 ))
-                .andExpect(jsonPath("$.id", (notNullValue()))) // (5)
-                .andExpect(jsonPath("$.title", (notNullValue())))
-                .andExpect(jsonPath("$.author", (notNullValue())));
+                .andDo(document("book",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        resource(
+                                ResourceSnippetParameters.builder()
+                                        .description("책 Read")
+                                        .summary("책 정보")
+                                        .pathParameters(
+                                                parameterWithName("id").description("book unique id")
+                                        )
+                                        .responseFields(
+                                                fieldWithPath("id").description("book unique id"),
+                                                fieldWithPath("title").description("title"),
+                                                fieldWithPath("author").description("author")
+                                        ).build()
+                        )
+                ));
+        ;
+
     }
 
 }
